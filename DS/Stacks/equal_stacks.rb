112ac91297c026764717568578744d8a91869463
Ramesh_RV <Ramesh_RV@symantec.com>
2019-07-27 20:57:42 +0530

    Removing ruby solution for Equal Stacks

--- DS/Stacks/equal_stacks.rb
+++
@@ -1,8 +0,0 @@
-# Get the input
-# Yeah we don't need this variable since we are using ruby
-_var = gets
-# Define cylinder
-cylinder = []
-(0..2).each { |index| cylinder[index] = gets.strip.split(' ').map(&:to_i) }
-until [cylinder[0], cylinder[1]].all? { |x| x == cylinder[2] }
-end
